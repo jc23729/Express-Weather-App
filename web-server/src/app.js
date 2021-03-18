9f0f30ffcb2a97eea5d6e27af3efa3e4f65b2f56
Juan Cruz <jc23729@gmail.com>
2021-03-18 10:48:20 -0400

    add html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,19 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send(<h1>Hello express!</h1>)
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send(<h1>Help</h1>)
 })
 
 app.get('/about', (req, res) => {
-    res.send('About')
+    res.send(<h1>About</h1>)
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Your weather')
+    res.send(<h1>Your Weather</h1>)
 })
 
 app.listen(3000, () => {
